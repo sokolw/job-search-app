fead6458c4b7f1d13637b90e4d41ac09a3d8835b
sokolw <sokolw21@gmail.com>
2023-05-04 07:13:20 +0300

    feat: add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -11,4 +13,4 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
-}
+};
