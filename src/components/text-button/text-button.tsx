7aa8279c4144d3d3c447de2e0986416eae769e19
sokolw <sokolw21@gmail.com>
2023-05-05 09:52:02 +0300

    feat: add text button

---
+++ src/components/text-button/text-button.tsx
@@ -0,0 +1,37 @@
+import styles from './text-button.module.css';
+import greyCrossImg from './../../assets/icons/gray-cross.svg';
+import blue400CrossImg from './../../assets/icons/blue-400-cross.svg';
+import blue500CrossImg from './../../assets/icons/blue-500-cross.svg';
+import { useState } from 'react';
+
+export const TextButton = () => {
+  const [isHover, setIsHover] = useState(false);
+  const [isClick, setIsClick] = useState(false);
+
+  return (
+    <button
+      className={styles.textButton}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+      onMouseDown={() => setIsClick(true)}
+      onMouseUp={() => setIsClick(false)}
+    >
+      <span
+        className={`${styles.textButton__text} ${isHover && !isClick ? styles.textButton__text_hover : ''} ${
+          isHover && isClick ? styles.textButton__text_click : ''
+        }`}
+      >
+        Сбросить все
+      </span>
+      {(() => {
+        if (isHover && !isClick) {
+          return <img src={blue400CrossImg} alt='blue-cross' />;
+        }
+        if (isHover && isClick) {
+          return <img src={blue500CrossImg} alt='blue-cross' />;
+        }
+        return <img src={greyCrossImg} alt='grey-cross' />;
+      })()}
+    </button>
+  );
+};
