2bee8db7bd1835261242c30e79a769ac976574d0
sokolw <sokolw21@gmail.com>
2023-05-05 15:47:09 +0300

    feat: add save button

---
+++ src/components/save-button/save-button.tsx
@@ -0,0 +1,34 @@
+import styles from './save-button.module.css';
+import { useState } from 'react';
+import grayStarImg from './../../assets/icons/gray-star.svg';
+import blueStarImg from './../../assets/icons/blue-star.svg';
+import filledBlueStarImg from './../../assets/icons/filled-blue-star.svg';
+
+type SaveButtonType = {
+  saved: boolean;
+};
+
+export const SaveButton = (props: SaveButtonType) => {
+  const { saved } = props;
+  const [isHover, setIsHover] = useState(false);
+  const [isActive, setIsActive] = useState(saved);
+
+  return (
+    <div
+      className={styles.saveButton}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+      onClick={() => setIsActive(!isActive)}
+    >
+      {(() => {
+        if (isHover && !isActive) {
+          return <img src={blueStarImg} alt='star' />;
+        }
+        if (isActive) {
+          return <img src={filledBlueStarImg} alt='star' />;
+        }
+        return <img src={grayStarImg} alt='star' />;
+      })()}
+    </div>
+  );
+};
