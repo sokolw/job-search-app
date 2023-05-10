4001974f0d9c0d5fa7d5b369d2c50b2a4e9ba997
sokolw <sokolw21@gmail.com>
2023-05-10 21:55:20 +0300

    feat: add store to root

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { MainPage } from './pages/main';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <MainPage />
+    <Provider store={store}>
+      <MainPage />
+    </Provider>
   </React.StrictMode>
 );
