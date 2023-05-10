cbf131d99ddf7c1cb03db83a5d137032a5766b83
sokolw <sokolw21@gmail.com>
2023-05-10 21:46:34 +0300

    refactor: del unused hook

--- src/pages/main/main-page.tsx
+++ src/pages/main/main-page.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
 import { Header } from '../../components/header';
 import { VacancyContainerLayout } from '../../components/vacancy-container-layout';
 
 export const MainPage = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header></Header>
