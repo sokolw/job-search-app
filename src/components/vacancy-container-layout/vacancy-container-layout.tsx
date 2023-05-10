0966d3aacca325f780ca34c8ed63a47a335c69f2
sokolw <sokolw21@gmail.com>
2023-05-10 21:59:31 +0300

    feat: add components FilterContainer, SearchField, VacancyListWithPagination

--- src/components/vacancy-container-layout/vacancy-container-layout.tsx
+++ src/components/vacancy-container-layout/vacancy-container-layout.tsx
@@ -1,12 +1,18 @@
 import styles from './vacancy-container-layout.module.css';
+import { FilterContainer } from '../filter-container';
+import { SearchField } from '../search-field';
+import { VacancyListWithPagination } from '../vacancy-list-with-pagination';
 
 export const VacancyContainerLayout = () => {
-  return <div className={styles.vacancyContainerLayout}>
-    <div>
-      filter container
+  return (
+    <div className={`wrapper ${styles.vacancyContainerLayout}`}>
+      <div className={styles.vacancyFilterLayout}>
+        <FilterContainer></FilterContainer>
+      </div>
+      <div className={styles.vacancyListLayout}>
+        <SearchField></SearchField>
+        <VacancyListWithPagination></VacancyListWithPagination>
+      </div>
     </div>
-    <div>
-      search container
-    </div>
-  </div>;
+  );
 };
