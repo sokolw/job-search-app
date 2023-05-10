adaf758b6d6ba64856d3e06e12bfd69f7ab351d2
sokolw <sokolw21@gmail.com>
2023-05-10 22:01:07 +0300

    feat: add interact with state filter, add add css border

--- src/components/filter-container/filter-container.tsx
+++ src/components/filter-container/filter-container.tsx
@@ -3,33 +3,89 @@ import { Dropdown } from '../dropdown';
 import { InputNumberWithControls } from '../input-number-with-controls';
 import { TextButton } from '../text-button';
 import { PrimaryButton } from '../primary-button';
-
-const industryType = [
-  'IT, интернет, связь, телеком',
-  'Кадры, управление персоналом',
-  'Искусство, культура, развлечения',
-  'Банки, инвестиции, лизинг',
-  'Дизайн',
-  'Машиностроение',
-  'Модель',
-];
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import {
+  resetWithoutKeyword,
+  selectWithoutKeyword,
+  setCatalogIndustry,
+  setPaymentFrom,
+  setPaymentTo,
+  validateFilter,
+} from '../../redux/filter-reducer';
+import { useEffect } from 'react';
+import { getCataloguesData, selectCatalogues, selectCataloguesStatus } from '../../redux/catalogues-reducer';
+import { getVacanciesData } from '../../redux/vacancies-reducer';
+import { Status } from '../../redux/status.enum';
 
 export const FilterContainer = () => {
+  const cataloguesStatus = useAppSelector(selectCataloguesStatus);
+  const catalogues = useAppSelector(selectCatalogues);
+
+  const { catalogIndustryTitle, catalogIndustryKey, paymentFrom, paymentTo, validationErrors, isSubmitted } =
+    useAppSelector(selectWithoutKeyword);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (cataloguesStatus === Status.IDLE) {
+      dispatch(getCataloguesData());
+    }
+
+    if (validationErrors.length === 0 && isSubmitted) {
+      dispatch(getVacanciesData({}));
+    }
+  }, [cataloguesStatus, validationErrors]);
+
+  const handlerPaymentFrom = (value: number) => {
+    dispatch(setPaymentFrom(value));
+  };
+
+  const handlerPaymentTo = (value: number) => {
+    dispatch(setPaymentTo(value));
+  };
+
+  const handlerApplyButton = () => {
+    dispatch(validateFilter());
+  };
+
+  const handlerCatalogIndustries = (title: string, key: number) => {
+    dispatch(setCatalogIndustry({ title, key }));
+  };
+
+  const handlerResetFilterButton = () => {
+    dispatch(resetWithoutKeyword());
+  };
+
+  console.log(catalogIndustryTitle, catalogIndustryKey, paymentFrom, paymentTo, validationErrors, isSubmitted);
+
   return (
     <div className={styles.filterContainer}>
       <div className={styles.filterContainer__titleGroup}>
         <h2 className={styles.filterContainer__title}>Фильтры</h2>
-        <TextButton></TextButton>
+        <TextButton callback={handlerResetFilterButton}></TextButton>
       </div>
       <div className={styles.filterContainer__industryGroup}>
-        <Dropdown options={industryType}></Dropdown>
+        <Dropdown
+          status={cataloguesStatus}
+          selectedTitle={catalogIndustryTitle}
+          selectedKey={catalogIndustryKey}
+          options={catalogues}
+          callback={handlerCatalogIndustries}
+        ></Dropdown>
       </div>
       <div className={`${styles.filterContainer__salaryGroup} ${styles.salary}`}>
         <h3 className={styles.salary__title}>Оклад</h3>
-        <InputNumberWithControls placeholder='От'></InputNumberWithControls>
-        <InputNumberWithControls placeholder='До'></InputNumberWithControls>
+        <InputNumberWithControls
+          currValue={paymentFrom}
+          callback={handlerPaymentFrom}
+          placeholder='От'
+        ></InputNumberWithControls>
+        <InputNumberWithControls
+          currValue={paymentTo}
+          callback={handlerPaymentTo}
+          placeholder='До'
+        ></InputNumberWithControls>
       </div>
-      <PrimaryButton width={275} height={40} content={'Применить'}></PrimaryButton>
+      <PrimaryButton width={275} height={40} content={'Применить'} callback={handlerApplyButton}></PrimaryButton>
     </div>
   );
 };
