e83ef3a14de82c3cbfdd839f0cd8e73c9b6b288c
sokolw <sokolw21@gmail.com>
2023-05-10 21:58:26 +0300

    feat: add interact with state

--- src/components/search-field/search-field.tsx
+++ src/components/search-field/search-field.tsx
@@ -2,14 +2,18 @@ import styles from './search-field.module.css';
 import { PrimaryButton } from '../primary-button';
 import lensImg from './../../assets/icons/lens.svg';
 import { useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { selectKeyword, setKeyword } from '../../redux/filter-reducer';
+import { getVacanciesData } from '../../redux/vacancies-reducer';
 
-type SearchFieldType = {
-  placeholder: string;
-};
-
-export const SearchField = (props: SearchFieldType) => {
-  const { placeholder } = props;
+export const SearchField = () => {
+  const keyword = useAppSelector(selectKeyword);
+  const dispatch = useAppDispatch();
   const [isFocus, setIsFocus] = useState(false);
+
+  const handleSearch = () => {
+    dispatch(getVacanciesData({}));
+  };
 
   return (
     <div className={`${styles.searchField} ${isFocus ? styles.searchField_focus : ''}`}>
@@ -17,11 +21,13 @@ export const SearchField = (props: SearchFieldType) => {
       <input
         className={styles.searchField__input}
         type='text'
-        placeholder={placeholder}
+        value={keyword}
+        placeholder={'Введите название вакансии'}
         onFocus={() => setIsFocus(true)}
         onBlur={() => setIsFocus(false)}
+        onInput={(event) => dispatch(setKeyword(event.currentTarget.value))}
       />
-      <PrimaryButton width={83} height={32} content={'Поиск'}></PrimaryButton>
+      <PrimaryButton width={83} height={32} content={'Поиск'} callback={handleSearch}></PrimaryButton>
     </div>
   );
 };
