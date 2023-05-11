310f578c60729e7666ae234aad77c22980505005
sokolw <sokolw21@gmail.com>
2023-05-11 09:37:02 +0300

    feat: move header

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,10 +1,8 @@
-import { Header } from '../../components/header';
 import { VacancyContainerLayout } from '../../components/vacancy-container-layout';
 
 export const MainPage = () => {
   return (
     <>
-      <Header></Header>
       <VacancyContainerLayout></VacancyContainerLayout>
     </>
   );
