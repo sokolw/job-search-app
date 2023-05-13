8bcbe9f4b4ab806bd658f64bec7542756757ce3d
sokolw <sokolw21@gmail.com>
2023-05-13 11:14:26 +0300

    feat: add navLink in Navigation component

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,10 +1,22 @@
+import { NavLink } from 'react-router-dom';
 import styles from './navigation.module.css';
 
 export const Navigation = () => {
+  const handlerNavLinkStyles = ({ isActive, isPending }: { isActive: boolean; isPending: boolean }) =>
+    isPending
+      ? styles.navigation__text
+      : isActive
+      ? `${styles.navigation__text} ${styles.navigation__text_active}`
+      : styles.navigation__text;
+
   return (
     <div className={styles.navigation}>
-      <span className={`${styles.navigation__text} ${styles.navigation__text_active}`}>Поиск Вакансий</span>
-      <span className={styles.navigation__text}>Избранное</span>
+      <NavLink to={``} className={handlerNavLinkStyles}>
+        Поиск Вакансий
+      </NavLink>
+      <NavLink to={`favorites`} className={handlerNavLinkStyles}>
+        Избранное
+      </NavLink>
     </div>
   );
 };
