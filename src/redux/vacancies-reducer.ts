7dfc04db48be69c8f2830c8de92fffeb9df39dd0
sokolw <sokolw21@gmail.com>
2023-05-13 11:16:08 +0300

    feat: add getFavoriteVacanciesData, change logic add/remove favs

--- src/redux/vacancies-reducer.ts
+++ src/redux/vacancies-reducer.ts
@@ -1,11 +1,12 @@
 import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { QueryVacancies, superjobApi } from '../api/superjob-api';
+import { QueryVacancies, formattingQueryGetByIds, superjobApi } from '../api/superjob-api';
 import { Vacancy, VacancyResponse } from '../common/types/vacancy-response';
 import { RootState } from './store';
 import { selectFilterQuery } from './filter-reducer';
 import { AxiosError } from 'axios';
 import { getAuthData } from './auth-reducer';
 import { RETRIES } from './constants';
+import { Status, StatusType } from './status.enum';
 
 const VACANCIES_DATA_KEY = 'VACANCIES_DATA';
 export const DEFAULT_ITEMS_PER_PAGE = 4;
@@ -14,23 +15,29 @@ export type CustomVacancy = Vacancy & { isFavorite: boolean };
 type VacanciesLocalStoreType = { favoriteVacancyIds: number[] };
 
 export interface VacanciesState {
-  status: 'idle' | 'loading' | 'succeeded' | 'failed';
+  status: StatusType;
   numberOfRetries: number;
   error: string | null | undefined;
   data: CustomVacancy[];
   totalVacancies: number;
   favoriteVacancyIds: number[];
+  statusFavoriteVacancies: StatusType;
+  favoriteVacancies: CustomVacancy[];
+  heapVacancies: CustomVacancy[];
   currentPage: number;
 }
 
 const initState = (): VacanciesState => {
   const initialState: VacanciesState = {
-    status: 'idle',
+    status: Status.IDLE,
     numberOfRetries: RETRIES,
     error: null,
     data: [],
     totalVacancies: 0,
     favoriteVacancyIds: [],
+    statusFavoriteVacancies: Status.IDLE,
+    favoriteVacancies: [],
+    heapVacancies: [],
     currentPage: -1,
   };
   const data = localStorage.getItem(VACANCIES_DATA_KEY);
@@ -84,26 +91,82 @@ export const getVacanciesData = createAsyncThunk(
   }
 );
 
+export const getFavoriteVacanciesData = createAsyncThunk(
+  'getFavoriteVacanciesData',
+  async (arg, { getState, rejectWithValue, dispatch }) => {
+    const state = getState() as RootState;
+    try {
+      const vacancyIds = selectFavoriteVacancyIds(state);
+      if (vacancyIds.length === 0) {
+        return { objects: [] } as unknown as VacancyResponse;
+      }
+      const reqQuery = formattingQueryGetByIds(vacancyIds);
+
+      const response = await superjobApi.getVacancies(reqQuery, {
+        Authorization: `Bearer ${state.auth.data.access_token}`,
+      });
+      return response.data as unknown as VacancyResponse;
+    } catch (error) {
+      const err = error as AxiosError;
+      if (!err.response) {
+        throw error;
+      }
+      const { response } = err;
+
+      if (response.status >= 400 && response.status < 500) {
+        dispatch(getAuthData());
+        return rejectWithValue(err.response.data);
+      }
+
+      if ((response.status >= 300 && response.status < 400) || response.status >= 500) {
+        const numberOfRetries = selectNumberOfRetries(state);
+        if (numberOfRetries > 0) {
+          dispatch(setNumberOfRetries(numberOfRetries - 1));
+          dispatch(getFavoriteVacanciesData());
+          return null;
+        }
+      }
+
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
+
 const vacanciesSlice = createSlice({
   name: 'vacancies',
   initialState: initState(),
   reducers: {
     addToFavoriteVacancies: (state, action: PayloadAction<{ id: number }>) => {
-      state.favoriteVacancyIds.push(action.payload.id);
-      localStorage.setItem(
-        VACANCIES_DATA_KEY,
-        JSON.stringify({ favoriteVacancyIds: state.favoriteVacancyIds } as VacanciesLocalStoreType)
-      );
+      const { id } = action.payload;
+      const vacancy = state.data.find((item) => item.id === id);
+      if (vacancy) {
+        vacancy.isFavorite = true;
+        state.favoriteVacancyIds.push(id);
+        localStorage.setItem(
+          VACANCIES_DATA_KEY,
+          JSON.stringify({ favoriteVacancyIds: state.favoriteVacancyIds } as VacanciesLocalStoreType)
+        );
+        if (state.statusFavoriteVacancies === Status.SUCCEEDED) {
+          state.favoriteVacancies.push(vacancy);
+        }
+      }
     },
     removeFromFavoriteVacancies: (state, action: PayloadAction<{ id: number }>) => {
-      state.favoriteVacancyIds = state.favoriteVacancyIds.filter((id) => id !== action.payload.id);
+      const { id } = action.payload;
+      state.favoriteVacancyIds = state.favoriteVacancyIds.filter((favoriteId) => favoriteId !== id);
+      state.favoriteVacancies = state.favoriteVacancies.filter((item) => {
+        if (item.id !== id) {
+          return true;
+        }
+        if (!state.heapVacancies.find((item) => item.id === id)) {
+          state.heapVacancies.push(item);
+        }
+      });
+      state.data = state.data.map((item) => (item.id !== id ? item : { ...item, isFavorite: false }));
       localStorage.setItem(
         VACANCIES_DATA_KEY,
         JSON.stringify({ favoriteVacancyIds: state.favoriteVacancyIds } as VacanciesLocalStoreType)
       );
-    },
-    setStatusManual: (state, action: PayloadAction<Pick<VacanciesState, 'status'>>) => {
-      state.status = action.payload.status;
     },
     setCurrentPage: (state, action: PayloadAction<number>) => {
       state.currentPage = action.payload;
@@ -114,8 +177,9 @@ const vacanciesSlice = createSlice({
   },
   extraReducers(builder) {
     builder
+      // getVacanciesData
       .addCase(getVacanciesData.pending, (state, action) => {
-        state.status = 'loading';
+        state.status = Status.LOADING;
       })
       .addCase(getVacanciesData.fulfilled, (state, action) => {
         if (action.payload) {
@@ -126,27 +190,58 @@ const vacanciesSlice = createSlice({
           state.data = customVacancies;
           state.totalVacancies = action.payload.total;
           state.numberOfRetries = RETRIES;
-          state.status = 'succeeded';
+          state.status = Status.SUCCEEDED;
         }
       })
       .addCase(getVacanciesData.rejected, (state, action) => {
-        state.status = 'failed';
+        state.status = Status.FAILED;
+        state.error = action.error.message;
+      })
+      // getFavoriteVacanciesData
+      .addCase(getFavoriteVacanciesData.pending, (state, action) => {
+        state.statusFavoriteVacancies = Status.LOADING;
+      })
+      .addCase(getFavoriteVacanciesData.fulfilled, (state, action) => {
+        if (action.payload) {
+          const customVacancies: CustomVacancy[] = action.payload.objects.map((item) => ({
+            ...item,
+            isFavorite: true,
+          }));
+          state.favoriteVacancies = customVacancies;
+          state.numberOfRetries = RETRIES;
+          state.statusFavoriteVacancies = Status.SUCCEEDED;
+        }
+      })
+      .addCase(getFavoriteVacanciesData.rejected, (state, action) => {
+        state.statusFavoriteVacancies = Status.FAILED;
         state.error = action.error.message;
       });
   },
 });
 
 export const vacanciesReducer = vacanciesSlice.reducer;
-export const {
-  addToFavoriteVacancies,
-  removeFromFavoriteVacancies,
-  setStatusManual,
-  setCurrentPage,
-  setNumberOfRetries,
-} = vacanciesSlice.actions;
+export const { addToFavoriteVacancies, removeFromFavoriteVacancies, setCurrentPage, setNumberOfRetries } =
+  vacanciesSlice.actions;
 // selectors
 export const selectVacanciesStatus = (state: RootState): string => state.vacancies.status;
 export const selectVacancies = (state: RootState): CustomVacancy[] => state.vacancies.data;
 export const selectTotalVacancies = (state: RootState): number => state.vacancies.totalVacancies;
 export const selectCurrentPage = (state: RootState): number => state.vacancies.currentPage;
 export const selectNumberOfRetries = (state: RootState): number => state.vacancies.numberOfRetries;
+export const selectVacancyById = (id: number) => {
+  return (state: RootState): CustomVacancy | undefined => {
+    const findCallback = (item: CustomVacancy) => item.id === id;
+    const fromData = state.vacancies.data.find(findCallback);
+    if (fromData) {
+      return fromData;
+    }
+    const fromFavorites = state.vacancies.favoriteVacancies.find(findCallback);
+    if (fromFavorites) {
+      return fromFavorites;
+    }
+    return state.vacancies.heapVacancies.find(findCallback);
+  };
+};
+export const selectFavoriteVacancyIds = (state: RootState): number[] => state.vacancies.favoriteVacancyIds;
+export const selectStatusFavoriteVacancies = (state: RootState): Status => state.vacancies.statusFavoriteVacancies;
+export const selectFavoriteVacancies = (state: RootState): CustomVacancy[] => state.vacancies.favoriteVacancies;
